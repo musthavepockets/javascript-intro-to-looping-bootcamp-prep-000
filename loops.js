12469c9ce36b27e7791656aeffbc104e02c71e3e
cd <coraldarling@outlook.com>
2018-07-19 01:11:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,14 @@
+function forLoop(array) {
+  for (let i = 0, i <= 25; i++) {
+    if (i = 1) {
+      array.push("I am 1 strange loop.");
+    }
+    else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  retrun array
+}
 
 function whileLoop(n) {
 let countdown = n
@@ -6,4 +17,13 @@ while (countdown > 0) {
   console.log(--countdown)
 }
 return ('done')
+}
+
+function doWhileLoop(array) {
+	function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do [array]
+
+while (maybeTrue())
 }
