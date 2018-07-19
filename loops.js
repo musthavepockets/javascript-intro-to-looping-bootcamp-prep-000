c0b10276beb4a4f2d0084dc6a1c147e69d533a23
cd <coraldarling@outlook.com>
2018-07-19 01:18:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
-  for (let i = 0, i <= 25; i++) {
+  for (let i = 0, i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.");
     }
     else {
-      array.push(`I am ${i} strange loops.`);
+      array.push(`"I am ${i} strange loops."`);
     }
   }
   return array
@@ -18,4 +18,14 @@ while (countdown > 0) {
   console.log(--countdown)
 }
 return ('done')
+}
+
+
+function doWhileLoop(array) {
+	function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do [array]
+
+while (maybeTrue())
 }
